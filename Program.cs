384580145a66fb098c4eb4e357c45e3fecb954f8
Rachana R <rachanarrachu28@gmail.com>
2020-10-21 23:05:22 +0530

    [Dhanya]Add . Added Total Wages For A Month

--- Program.cs
+++ Program.cs
@@ -7,28 +7,35 @@ namespace EmployeeWageComputation
         public static int WAGE_PER_HR = 20;
         public const int PART_TIME = 2;
         public const int FULL_TIME = 1;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             int salary, workingHr = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalSalary = 0;
+            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Full Time Employee");
-                    workingHr = 8;
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Part Time Employee");
-                    workingHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("Absent");
-                    workingHr = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Full Time Employee");
+                        workingHr = 8;
+                        break;
+                    case PART_TIME:
+                        Console.WriteLine("Part Time Employee");
+                        workingHr = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        workingHr = 0;
+                        break;
+                }
+                salary = workingHr * WAGE_PER_HR;
+                totalSalary += salary;
+                Console.WriteLine("Employee Wage: " + salary);
             }
-            salary = workingHr * WAGE_PER_HR;
-            Console.WriteLine("Employee Wage: " + salary);
+            Console.WriteLine("The total salary is " +totalSalary);
         }
     }
 }

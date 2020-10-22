00c1a6333d00e359719efaa40f2838babe46ae19
Dhanya S V <dhanyavirupaksha@gmail.com>
2020-10-22 18:12:11 +0530

    [Dhanya]Add . Added Employee Wage For Multiple Companies

--- Program.cs
+++ Program.cs
@@ -4,16 +4,15 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        public static int WAGE_PER_HR = 20;
+       
         public const int PART_TIME = 2;
         public const int FULL_TIME = 1;
-        public const int NUMBER_OF_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 100;
-        static void Main(string[] args)
+        
+        public static void ComputeEmployeeWage(string company, int wagePerHr, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            int salary=0, totalWorkingHr = 0, totalWorkingDays = 0;
-            int totalSalary = 0, workingHr;
-            while (totalWorkingHr < MAX_WORKING_HRS && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
+            int totalWorkingHr = 0, totalWorkingDays = 0;
+            int totalSalary = 0, workingHr=0;
+            while (totalWorkingHr < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -34,11 +33,15 @@ namespace EmployeeWageComputation
                         break;
                 }
                 totalWorkingHr = totalWorkingHr + workingHr;
-                //Console.WriteLine("Days:" + totalWorkingDays +"  " + "EmployeeWorkingHrs:" + workingHr);
+                Console.WriteLine("Days:" + totalWorkingDays +"  " + "EmployeeWorkingHrs:" + workingHr);
             }
-            totalSalary = totalWorkingHr * WAGE_PER_HR;
-            Console.WriteLine("totalWorkingDays:" +totalWorkingDays +" " +"totalWorkingHr:" 
-                +totalWorkingHr +"  " +"totalSalary:" + totalSalary);
+            totalSalary = totalWorkingHr * wagePerHr;
+            Console.WriteLine( "The Employee Wage for a Company:" + company +" is " + totalSalary);
+        }
+        public static void Main(string[] arg)
+        {
+            ComputeEmployeeWage("HappiestMind", 20, 2, 10);
+            ComputeEmployeeWage("BridgeLabz", 20, 2, 10);
         }
     }
 }
